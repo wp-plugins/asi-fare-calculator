6b78c9c4cde33bdc0f5c1d4bee5a69fcf8f7ecee
Adaptive Solution <Adaptive Solution@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-04 12:15:34 +0000

    text missing
    git-svn-id: https://plugins.svn.wordpress.org/asi-fare-calculator/trunk@1174381 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/asi_script.js
+++ js/asi_script.js
@@ -32,9 +32,14 @@ jQuery(document).ready(function()
 
 function doCalculation()
 {
-
+	var cartypes=document.getElementById('cartypes').value;
     var address = document.getElementById('source').value;
     var destination = document.getElementById('destination').value;
+    if(cartypes.trim()=='')
+    {
+        alert("Please Select Car Type. if Cartype is empty, then you need to inser first from admin side.");
+        return false;
+    }
     if(address.trim() == '') {
         alert("Please Enter Pickup Address");
         source = '';
@@ -228,8 +233,3 @@ jQuery(document).on('click','.rem',function(){
     //alert(data);
     	         }); 
 });
-
-function addcartype()
-{
-    alert('Please Do not wrtie currency symbol it already hase been written'); return false;
-}
